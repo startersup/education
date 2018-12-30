c0f4e1839a5f7eb27a33df6a036df1406818546a
vinodhan16 <vinodhan.thunderbolt@gmail.com>
2018-12-31 03:31:13 +0530

    Fix aptitude page

--- aptitude.php
+++ aptitude.php
@@ -1,4 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
 <title>Engineering Kit</title>
+        <link rel="icon" href="./assets/images/speed.png" type="image/gif" sizes="16x16">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans|Roboto:500" rel="stylesheet">
+<link rel="stylesheet" href="./assets/css/style.css">
+  <script src="./assets/js/sidenav.js"></script>  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans|Roboto:500" rel="stylesheet">
+  <link rel="stylesheet" href="./assets/css/nav.css">
+      <script src="./assets/js/sidenav.js"></script> 
+</head>
+<body>
     <?php include 'navbar.html' ?>
     <section class="main" id="main">
         <div class="container">
@@ -53,7 +72,7 @@
             </div>
                <div class="col-md-4">
                      <div class="card hover jobs"><h4>Verbal Ability.</h4>
-            <p>Cognizant Technology Solutions India Ltd </p>
+            <p>Topics Covered </p>
                 <ul>
                 <li>Reading Comprehension</li>
                 <li>Jumbled sentences</li>  
