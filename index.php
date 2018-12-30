7ef88ff5d9e1d75a635e28bd639ea6f6983855c0
Saicharan <saicharan14996@gmail.com>
2018-12-30 21:40:05 +0530

    Update index.php
    s

--- index.php
+++ index.php
@@ -63,13 +63,7 @@
         </div>
              </div>
     </section>
-        <div class="icon-bar hidden-xs">
-  <a href="#" class="facebook"><i class="fa fa-facebook"></i></a> 
-  <a href="#" class="twitter"><i class="fa fa-twitter"></i></a> 
-  <a href="#" class="google"><i class="fa fa-google"></i></a> 
-  <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
-  <a href="#" class="youtube"><i class="fa fa-youtube"></i></a> 
-</div>
+       
     </div>
     <script>
 $(function() {
