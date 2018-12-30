b14c255a46388b374fc3bb03e14154829d11601c
Saicharan <saicharan14996@gmail.com>
2018-12-31 00:03:27 +0530

    ss
    sss

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+<title>Engineering Kit</title>
+        <link rel="icon" href="./assets/images/speed.png" type="image/gif" sizes="16x16">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans|Roboto:500" rel="stylesheet">
+<link rel="stylesheet" href="./assets/css/style.css">
+  <script src="./assets/js/sidenav.js"></script>  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans|Roboto:500" rel="stylesheet">
+  <link rel="stylesheet" href="./assets/css/nav.css">
+      <script src="./assets/js/sidenav.js"></script> 
+</head>
+<body>
+<?php  include("navbar.html")  ?>
     <div class="preload"><p style="float:inherit;font-size:20px;margin-left:5%;">Preparing Kit for you.....</p><img style="width:350px;" src="./assets/images/book-gif.gif">
 </div>
     <div class="content">
-<title>Engineering Kit</title>
-        <link rel="icon" href="./assets/images/speed.png" type="image/gif" sizes="16x16">
-<?php  include("navbar.html")  ?>
-
     <section class="main" id="main">
         <div class="container">
     <div class="row">
