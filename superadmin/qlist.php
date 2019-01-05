bed8c61149fbaaaf6dd6b7c446ad8d57b95e9bcd
Saicharan <saicharan14996@gmail.com>
2019-01-05 13:21:10 +0530

    admin table
    new update in admin

---
+++ superadmin/qlist.php
@@ -0,0 +1,192 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Engineering Kit</title>
+    <link rel="icon" href="../assets/images/speed.png" type="image/gif" sizes="16x16">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans|Roboto:500" rel="stylesheet">
+        <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+<link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/nav.css">
+    <link rel="stylesheet" href="./css/admin.css">
+  <script src="./js/side.js"></script>  
+</head>
+<body>
+<div class="navbar navbar-default navbar-fixed-top navbar-expand-lg navbar-light">
+    <div class="admin">
+        <center><h4> Admin Dashboard</h4></center>
+  </div></div>
+
+    <div id="mySidebar" class="sidepanel" >
+  <a href="admin.html">Dashboard</a>
+   <a href="pages.html">Pages List</a>
+    <a href="uer.html">Web Userlist</a> 
+    <a href="jobs.html">Jobs List</a> 
+     <a href="eventlist.html">Events List</a> 
+    <a href="load.php">Upload Data</a>   
+         <a href="qlist.php">Questions</a>  
+        <a href="settings.html">Settings</a>  
+</div>
+
+    <section class="main" id="main">
+
+<div class="container">
+	<div class="row">
+	<table id="example" class="table table-striped table-bordered" style="width:100%">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Question</th>
+                <th>Answer</th>
+                <th>options</th>
+                <th>Explanation</th>
+                  <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>134</td>
+                <td> A number when divided by a divisor leaves a remainder of 24.When twice the original number is divided by the same divisor, the remainder is 11. What is the value of the divisor?</td>
+                 <td>20</td>
+                <td>13 15 20 25</td>
+                 <td>bla bla bla bla bla bla bla</td>
+                <td><button class="btn btn-success">Update</button></td>
+            </tr>
+                 <tr>
+                <td>134</td>
+                <td> A number when divided by a divisor leaves a remainder of 24.When twice the original number is divided by the same divisor, the remainder is 11. What is the value of the divisor?</td>
+                 <td>20</td>
+                <td>13 15 20 25</td>
+                 <td>bla bla bla bla bla bla bla</td>
+                <td><button class="btn btn-success">Update</button></td>
+            </tr>
+             <tr>
+                <td>134</td>
+                <td> A number when divided by a divisor leaves a remainder of 24.When twice the original number is divided by the same divisor, the remainder is 11. What is the value of the divisor?</td>
+                 <td>20</td>
+                <td>13 15 20 25</td>
+                 <td>bla bla bla bla bla bla bla</td>
+                <td><button class="btn btn-success">Update</button></td>
+            </tr>
+             <tr>
+                <td>134</td>
+                <td> A number when divided by a divisor leaves a remainder of 24.When twice the original number is divided by the same divisor, the remainder is 11. What is the value of the divisor?</td>
+                 <td>20</td>
+                <td>13 15 20 25</td>
+                 <td>bla bla bla bla bla bla bla</td>
+                <td><button class="btn btn-success">Update</button></td>
+            </tr>
+             <tr>
+                <td>134</td>
+                <td> A number when divided by a divisor leaves a remainder of 24.When twice the original number is divided by the same divisor, the remainder is 11. What is the value of the divisor?</td>
+                 <td>20</td>
+                <td>13 15 20 25</td>
+                 <td>bla bla bla bla bla bla bla</td>
+                <td><button class="btn btn-success">Update</button></td>
+            </tr>
+             <tr>
+                <td>134</td>
+                <td> A number when divided by a divisor leaves a remainder of 24.When twice the original number is divided by the same divisor, the remainder is 11. What is the value of the divisor?</td>
+                 <td>20</td>
+                <td>13 15 20 25</td>
+                 <td>bla bla bla bla bla bla bla</td>
+                <td><button class="btn btn-success">Update</button></td>
+            </tr>
+             <tr>
+                <td>134</td>
+                <td> A number when divided by a divisor leaves a remainder of 24.When twice the original number is divided by the same divisor, the remainder is 11. What is the value of the divisor?</td>
+                 <td>20</td>
+                <td>13 15 20 25</td>
+                 <td>bla bla bla bla bla bla bla</td>
+                <td><button class="btn btn-success">Update</button></td>
+            </tr>
+             <tr>
+                <td>134</td>
+                <td> A number when divided by a divisor leaves a remainder of 24.When twice the original number is divided by the same divisor, the remainder is 11. What is the value of the divisor?</td>
+                 <td>20</td>
+                <td>13 15 20 25</td>
+                 <td>bla bla bla bla bla bla bla</td>
+                <td><button class="btn btn-success">Update</button></td>
+            </tr>
+             <tr>
+                <td>134</td>
+                <td> A number when divided by a divisor leaves a remainder of 24.When twice the original number is divided by the same divisor, the remainder is 11. What is the value of the divisor?</td>
+                 <td>20</td>
+                <td>13 15 20 25</td>
+                 <td>bla bla bla bla bla bla bla</td>
+                <td><button class="btn btn-success">Update</button></td>
+            </tr> <tr>
+                <td>134</td>
+                <td> A number when divided by a divisor leaves a remainder of 24.When twice the original number is divided by the same divisor, the remainder is 11. What is the value of the divisor?</td>
+                 <td>20</td>
+                <td>13 15 20 25</td>
+                 <td>bla bla bla bla bla bla bla</td>
+                <td><button class="btn btn-success">Update</button></td>
+            </tr>
+            <tr>
+                <td>134</td>
+                <td> A number when divided by a divisor leaves a remainder of 24.When twice the original number is divided by the same divisor, the remainder is 11. What is the value of the divisor?</td>
+                 <td>20</td>
+                <td>13 15 20 25</td>
+                 <td>bla bla bla bla bla bla bla</td>
+                <td><button class="btn btn-success">Update</button></td>
+            </tr>
+            
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>Id</th>
+                <th>Question</th>
+                <th>Answer</th>
+                <th>options</th>
+                <th>Explanation</th>
+                  <th>Action</th>
+            </tr>
+        </tfoot>
+    </table>
+	</div>
+</div>
+    </section>
+<style>table{
+    width:100%;
+}
+#example_filter{
+    float:right;
+}
+#example_paginate{
+    float:right;
+}
+label {
+    display: inline-flex;
+    margin-bottom: .5rem;
+    margin-top: .5rem;
+   
+}</style>
+    <script>$(document).ready(function() {
+    $('#example').DataTable(
+        
+         {     
+
+      "aLengthMenu": [[5, 10, 25, -1], [5, 10, 25, "All"]],
+        "iDisplayLength": 5
+       } 
+        );
+} );
+
+
+function checkAll(bx) {
+  var cbs = document.getElementsByTagName('input');
+  for(var i=0; i < cbs.length; i++) {
+    if(cbs[i].type == 'checkbox') {
+      cbs[i].checked = bx.checked;
+    }
+  }
+}</script>
+    </body></html>
