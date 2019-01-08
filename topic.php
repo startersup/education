c2a294f793118f25b7b30ad7b663c9c8b1f84cb8
Saicharan <saicharan14996@gmail.com>
2019-01-08 20:55:13 +0530

    Update topic.php
    dd

--- topic.php
+++ topic.php
@@ -159,7 +159,7 @@ include 'config.php';
                                     <div class='answer'>  <div class='display'><span>Asked in ".$row['appeared']."</span>
                                     </div>
                                         <p><b>Answer:</b> ".$row['answer']."</p>
-                                         <p><b>Explanation:</b></p><textarea style='font-size:15px;' readonly>".$row['explanation']."</textarea> 
+                                         <p><b>Explanation:</b></p><textarea style='font-size:15px;max-width:500px;width:100%;height:400px;height:100%;' readonly>".$row['explanation']."</textarea> 
                                     </div>";
                                     ?>
                                   <div class="commentbox">
