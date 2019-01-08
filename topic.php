3e024c12518def47f0a8c55490225b248b3fb52b
Saicharan <saicharan14996@gmail.com>
2019-01-08 21:02:21 +0530

    ss
    sss

--- topic.php
+++ topic.php
@@ -159,7 +159,7 @@ include 'config.php';
                                     <div class='answer'>  <div class='display'><span>Asked in ".$row['appeared']."</span>
                                     </div>
                                         <p><b>Answer:</b> ".$row['answer']."</p>
-                                         <p><b>Explanation:</b></p><textarea class="unedit" readonly>'.$row['explanation'].'</textarea> 
+                                         <p><b>Explanation:</b></p><textarea readonly>".$row['explanation']."</textarea> 
                                     </div>";
                                     ?>
                                   <div class="commentbox">
