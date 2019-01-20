c665eac75246e3345b0408001f0741510ed9444b
vinodhan16 <vinodhan.thunderbolt@gmail.com>
2019-01-20 14:31:17 +0530

    Update quest.php

--- quest.php
+++ quest.php
@@ -231,7 +231,7 @@ if ($conn->query($sql) === TRUE) {
                                 $numrows=mysqli_num_rows($query);
                                 $cat=preg_replace('#[^0-9a-zA-Z_-]#i', '', $_GET['cat']);
                                 $sub=preg_replace('#[^0-9a-zA-Z_-]#i', '', $_GET['sub']);
-                                if($numrows>=(($count+1)*5))
+                                if($numrows>(($count+1)*5))
                                 {
                                 $c=$count+1;
                                 echo"<center><a href='quest/".$cat."/".$sub."/".$c."'><button class='btn btn-successs'>Next</button></a></center>";
