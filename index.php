7d6e4033f401ba137feb07ab15cef57f78355d9f
Saicharan <saicharan14996@gmail.com>
2019-10-12 21:47:36 +0530

    Update index.php
    ddd

--- index.php
+++ index.php
@@ -205,8 +205,9 @@ mysqli_close($conn);
 
         <section class="background">
         <div class="mainspeech">
-            <div class="">
+            <div class="container">
             <div class="row">
+<!--
                 <div class="col-md-7">
           <h1>A Complete Guide for Engineering Students</h1>  
             <h3>A Forum that integrates all the betterments of students that to attain success.</h3>
@@ -214,6 +215,7 @@ mysqli_close($conn);
                 <p>Better clarity <span>|</span> Better Knowledge <span>|</span> Better Career</p>
                     <center><button class="readmore">Read More</button></center>
             </div>
+-->
                 <div class="col-md-5">
             <center><img class="mainimg" src="./assets/images/background-banner.png"></center>
                     </div>
